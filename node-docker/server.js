63f7cb5affbb8a279e60288d104e1869fd15e1d6
Michael Nguyen <michaelnguyen.creative@gmail.com>
2023-04-18 15:42:19 +0700

    refactor server definition code to use async logic & to include mongodb connection logic

--- node-docker/server.js
+++ node-docker/server.js
@@ -1,12 +1,26 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
-const requestListener = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello world')
+const startServer = async () => {
+  try {
+    // MongoDB connection logic
+    const { CONNECTIONSTRING } = process.env
+    console.log('connecting to MongoDB with', CONNECTIONSTRING)
+    await mongoose.connect(CONNECTIONSTRING)
+    console.log('connected to MongoDB')
+
+    // Node server definition
+    const requestListener = (req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end('Hello world')
+    }
+    const app = http.createServer(requestListener)
+
+    const PORT = 8000 // Bind node server to port
+    app.listen(PORT, () => console.log('Server is running on port', PORT))
+  } catch (error) {
+    console.log('An error occurred:', error)
+  }
 }
 
-const app = http.createServer(requestListener)
-
-const PORT = 8000
-app.listen(PORT)
-console.log('Server is running on port', PORT)
+startServer()
